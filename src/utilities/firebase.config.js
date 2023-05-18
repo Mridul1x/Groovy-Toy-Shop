0da91dfad4417bba6d5cf5e66db64a38f801d65d
Mridul1x <mridulhasannn@gmail.com>
2023-05-18 19:47:43 +0600

    All the package added and client site running checked

---
+++ src/utilities/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOBOUN3TdUAx_yAALnYz2N-gWPnpKhCeo",
+  authDomain: "assignment-11-toy-market-66168.firebaseapp.com",
+  projectId: "assignment-11-toy-market-66168",
+  storageBucket: "assignment-11-toy-market-66168.appspot.com",
+  messagingSenderId: "832477158361",
+  appId: "1:832477158361:web:ce84fa9842a15b8fae97c8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
