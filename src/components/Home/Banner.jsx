a8777f27b237aeff61ff8c0de9c9eace80902354
Mridul1x <mridulhasannn@gmail.com>
2023-05-19 03:15:36 +0600

    Banner Created

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,19 +1,46 @@
 import React from "react";
-import AwesomeSlider from "react-awesome-slider";
 import banner1 from "../../assets/banner1.png";
 import banner2 from "../../assets/banner2.png";
-import AwsSliderStyles from 'react-awesome-slider/src/styles.scss';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./Banner.css";
+import { Carousel } from "react-responsive-carousel";
 
 const Banner = () => {
   return (
-    <div>
-      <AwesomeSlider cssModule={AwsSliderStyles}>
-        <div data-src={banner1} />
-        <div data-src={banner2} />
-        <div data-src={banner1} />
-      </AwesomeSlider>
+    <div className="banner p-5 ">
+      <div className="justify-center text-center">
+        <h2 className="text-xl lg:text-3xl font-bold text-white">
+          Buy your favorite character action figures here!
+        </h2>
+        <p className="text-white my-3">
+          The most complete action figures More than 1000 collectible action
+          figure rady to collect
+        </p>
+        <button className="btn btn-warning">Shop Now</button>
+      </div>
+      <div className="mt-4 lg:w-2/3 mx-auto">
+        <Carousel interval={2000} infiniteLoop={true} autoPlay={true}>
+          <div className="image-slide">
+            <div className="image-container">
+              <img src={banner1} alt="Banner 2" />
+            </div>
+          </div>
+          <div className="image-slide">
+            <div className="image-container">
+              <img src={banner1} alt="Banner 2" />
+            </div>
+          </div>
+          <div className="image-slide">
+            <div className="image-container">
+              <img src={banner1} alt="Banner 3" />
+            </div>
+          </div>
+        </Carousel>
+      </div>
     </div>
   );
 };
 
 export default Banner;
+{
+}
